d2abd228cf89028b7e8165b6509223481455b36a
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-04-19 10:57:59 -0600

    started following the JS portion of the tutorial. got a lot set up. setting the class to add the mark. switch turns. hover the mark depending on the turn.

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,49 @@
-console.log('I am connected to the HTML!');
+// console.log('I am connected to the HTML!');
+
+const xClass = 'x';
+const circleClass = 'circle';
+const cellElements = document.querySelectorAll('[data-cell]');
+const board = document.getElementById('board');
+let circleTurn;
+
+startGame();
+
+function startGame() {
+  circleTurn = false;
+  cellElements.forEach((cell) => {
+    cell.addEventListener('click', handleClick, { once: true });
+  });
+  setBoardHoverClass();
+}
+
+function handleClick(e) {
+  const cell = e.target;
+  const currentClass = circleTurn ? circleClass : xClass;
+
+  //placeMark
+  placeMark(cell, currentClass);
+
+  //check for Win
+  //check for draw
+  //switch turns
+  swapTurns();
+  setBoardHoverClass();
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+function swapTurns() {
+  circleTurn = !circleTurn;
+}
+
+function setBoardHoverClass() {
+  board.classList.remove(xClass);
+  board.classList.remove(circleClass);
+  if (circleTurn) {
+    board.classList.add(circleClass);
+  } else {
+    board.classList.add(xClass);
+  }
+}
