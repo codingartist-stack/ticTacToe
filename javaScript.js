4088c8cb762f0ccffcc17dddcb5d87c790936ea6
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-04-19 11:51:37 -0600

    got the logic for winning and added a winning message.

--- javaScript.js
+++ javaScript.js
@@ -2,8 +2,20 @@
 
 const xClass = 'x';
 const circleClass = 'circle';
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 const cellElements = document.querySelectorAll('[data-cell]');
 const board = document.getElementById('board');
+const winningMessage = document.getElementById('winMessage');
+const winText = document.getElementById('winText');
 let circleTurn;
 
 startGame();
@@ -24,10 +36,21 @@ function handleClick(e) {
   placeMark(cell, currentClass);
 
   //check for Win
+  if (checkWin(currentClass)) {
+    endGame(false);
+  }
   //check for draw
   //switch turns
   swapTurns();
   setBoardHoverClass();
+}
+
+function endGame(draw) {
+  if (draw) {
+  } else {
+    winText.innerText = `${circleTurn ? "0's" : "X's"} Wins!`;
+  }
+  winningMessage.classList.add('show');
 }
 
 function placeMark(cell, currentClass) {
@@ -46,4 +69,12 @@ function setBoardHoverClass() {
   } else {
     board.classList.add(xClass);
   }
+}
+
+function checkWin(currentClass) {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
 }
