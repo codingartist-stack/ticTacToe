e386028101e57bbbffa393a29650544ffd1e61cb
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-04-19 14:39:16 -0600

    grabbed the elements that I know I would need using js.

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,9 @@
 console.log('I am connected! ');
 
+const gameBoard = document.getElementById('board');
+const winMessage = document.getElementById('winMessage');
+const winText = document.getElementById('winText');
+const resetButton = document.getElementById('restartButton');
 //create board
 //create players
 
@@ -9,3 +13,5 @@ console.log('I am connected! ');
 //check for draw
 
 //display results message
+
+//1. store the gameboard as an array inside of a gameboard object
