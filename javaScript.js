2f44db67183ef885cf09b17ef2240a206a1aeae9
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-04-19 18:13:20 -0600

    put a background on the gameboard div just so I could see where everything would be.

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,38 @@
 console.log('I am connected! ');
+//Your main goal here is to have as little global code as possible.
 
 const gameBoard = document.getElementById('board');
 const winMessage = document.getElementById('winMessage');
 const winText = document.getElementById('winText');
 const resetButton = document.getElementById('restartButton');
-//create board
-//create players
+
+//1. store the gameboard as an array inside of a gameboard object
+//module
+
+const TicTacToeApp = (() => {
+  const board = [];
+  for (let i = 0; i < 3; i++) {
+    board.push(new Array(3));
+  }
+
+  const addX = (i, j) => {
+    board[i][j] = 'X';
+  };
+
+  const addO = (i, j) => {
+    board[i][j] = 'O';
+  };
+
+  return {
+    board: board,
+    addX: addX,
+    addO: addO,
+  };
+})();
+
+//2. Your players are also going to be stored in objects,
+// and you’re probably going to want an object to control the flow of the game itself.
+//factories
 
 //place markers
 
@@ -13,5 +40,3 @@ const resetButton = document.getElementById('restartButton');
 //check for draw
 
 //display results message
-
-//1. store the gameboard as an array inside of a gameboard object
