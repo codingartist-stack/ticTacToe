1c531c6eb406edd4f762aa16d51be01fb1ddb71a
codingartist-stack <121696544+codingartist-stack@users.noreply.github.com>
2023-04-25 16:41:21 -0600

    deleted all the stuff Ben and worked on so I could rewrite it. hopefully I will figure it out.

--- javaScript.js
+++ javaScript.js
@@ -9,38 +9,9 @@ const resetButton = document.getElementById('restartButton');
 //1. store the gameboard as an array inside of a gameboard object
 //module
 
-const TicTacToeApp = (() => {
-  const board = [];
-  for (let i = 0; i < 3; i++) {
-    board.push(new Array(3));
-  }
-
-  const addX = (i, j) => {
-    board[i][j] = 'X';
-  };
-
-  const addO = (i, j) => {
-    board[i][j] = 'O';
-  };
-
-  for (const space of Object.values(board)) {
-    const square = document.createElement('div');
-    square.classList.add('squares');
-    // gameBoard.appendChild(square);
-  }
-
-  return {
-    board: board,
-    addX: addX,
-    addO: addO,
-  };
-})();
-
 //2. Your players are also going to be stored in objects,
 // and you’re probably going to want an object to control the flow of the game itself.
 //factories
-
-const players = (name, marker) => {};
 
 //place markers
 
